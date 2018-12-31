b310527898d36e32d3a6d38bff6cab92b7c4ed14
Jaspreet Singh <jaspreetjhass91@gmail.com>
2018-12-31 22:53:44 +0530

    added log 4j for Mybatis application

--- MyBatisHelloWorld/src/main/java/Executors/ProfileDAOExecutor.java
+++ MyBatisHelloWorld/src/main/java/Executors/ProfileDAOExecutor.java
@@ -7,14 +7,17 @@ import java.util.Date;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.log4j.Logger;
 
 import DAO.ProfileDAO;
 import beans.Profile;
 
 public class ProfileDAOExecutor {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	private static Logger LOGGER = Logger.getLogger(ProfileDAOExecutor.class);
 
+	public static void main(String[] args) throws FileNotFoundException {
+		LOGGER.trace("enter into method");
 		SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder()
 				.build(new FileInputStream("mybatis-config.xml"));
 		SqlSession session = sessionFactory.openSession(true);
@@ -24,7 +27,7 @@ public class ProfileDAOExecutor {
 		System.out.println("************************************************");
 		System.out.println(daoImpl.getProfile("amanBhandari"));
 		System.out.println("************************************************");
-		//to get the max profile id
+		// to get the max profile id
 		Long profileId = session.selectOne("getMaxId");
 		Profile profile = new Profile(profileId, "sahilHdr", "sahil", "singh", new Date());
 		daoImpl.addProfile(profile);
@@ -35,6 +38,7 @@ public class ProfileDAOExecutor {
 		System.out.println("************************************************");
 		System.out.println(daoImpl.getProfileCreatedDate("jaspreetjhass", 2018122901l));
 
+		LOGGER.trace("exit from method");
 	}
 
 }
